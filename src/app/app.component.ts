8231d39bfe3b208784f03a28e250400255e3a8d4
Sowmya <sowmya.aji@gmail.com>
2019-10-29 15:18:06 -0400

    Add functionality to switch between recipes & shopping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shopRecipe';
+  displayChoice = 'recipe'
+
+  onNavigate(choice: string) {
+    this.displayChoice = choice;
+  }
 }
